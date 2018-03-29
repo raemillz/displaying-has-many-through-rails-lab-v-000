fbb84b3b93fe29a92eb857ec993fd57410b55597
Rachel Miller <raeanne1991@gmail.com>
2018-03-29 18:04:57 +0000

    automatically backed up by learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,6 +2,7 @@ class AppointmentsController < ApplicationController
   def index
     @appointments = Appointment.all
   end
+  
   def show
     @doctor = Doctor.find(params[:id])
     @patient = Patient.find(params[:id])
