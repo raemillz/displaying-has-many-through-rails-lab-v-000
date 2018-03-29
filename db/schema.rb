06c5da3f9564f6976c8a667eda348672ef7aaf97
Rachel Miller <raeanne1991@gmail.com>
2018-03-29 17:46:24 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329174204) do
+ActiveRecord::Schema.define(version: 20180329174606) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointmet_datetime"
